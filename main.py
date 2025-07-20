a3956bc9c33feab377d6c772706b2029978536f8
Oksana581 <volosh581@gmail.com>
2025-07-20 12:43:15 +0200

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-from gastro_ai_bot_clean.config import settings
-from gastro_ai_bot_clean.scraping import site_scraper
-from gastro_ai_bot_clean.ai import content_generator
-from gastro_ai_bot_clean.telegram import bot_sender
-
-if __name__ == "__main__":
-    print("Запуск бота...")
